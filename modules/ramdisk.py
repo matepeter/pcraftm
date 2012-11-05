a2c3c54e06aba9ed549ef1e5c6a9fe61bf155dde
Felix Rauch <wurst@240815.de>
2012-11-05 10:09:09 +0000

    Put ismounted check back into module, smarter handling of getStatvfs return value in ramdisk.py

--- modules/ramdisk.py
+++ modules/ramdisk.py
@@ -1,7 +1,18 @@
 import os
-import subprocess
 from . import conf
 from __functions import *
 
 def getStatus ( ) :
-	return getStatvfs(conf.config['ramdisk_dir'])
+	failed = False
+	result = dict()
+
+	if not os.path.ismount(conf.config['ramdisk_dir']):
+		result['NOT_MOUNTED'] = True
+		failed = True
+
+	if not failed:
+		r = getStatvfs(conf.config['ramdisk_dir'])
+		for x in r.iterkeys():
+			result[x] = r[x]
+
+	return result
